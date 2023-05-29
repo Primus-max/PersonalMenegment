ba4064742e47697de615f8ac0145c24319a4a3f1
Vladimir <urkytsk3@gmail.com>
2023-05-29 09:32:36 +0800

    Добавил конвертер для отображения ЗП

---
+++ PersonnelManagement/ViewModel/Converters/SalaryConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PersonnelManagement.ViewModel.Converters
+{
+    public class SalaryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal salary)
+            {
+                // Форматирование значения зарплаты в удобочитаемый формат строки
+                return salary.ToString("C", culture).Replace("$", "₽"); // Используется формат "C" для форматирования в денежный формат
+            }
+
+            return value; // Возвращение исходного значения, если не удалось преобразовать
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // Обратное преобразование не требуется для отображения
+        }
+    }
+}
