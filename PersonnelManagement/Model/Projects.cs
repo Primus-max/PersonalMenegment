6695359e858d64bf0bf22f61d9b07315bfb11ea9
Vladimir <urkytsk3@gmail.com>
2023-05-30 07:36:32 +0800

    Рачет ProgressBar и вывод во view

--- PersonnelManagement/Model/Projects.cs
+++ PersonnelManagement/Model/Projects.cs
@@ -14,5 +14,22 @@ namespace PersonnelManagement.Model
         public decimal ProjectBudget { get; set; }
         public string ProjectManager { get; set; }
         public DateTime FinishedDate { get; set; }
+        public int IsActive { get; set; }
+
+        public int ProgressBarValue
+        {
+            get
+            {
+                DateTime currentDate = DateTime.Now;
+                TimeSpan projectDuration = FinishProject - StartProject;
+                TimeSpan elapsedDuration = currentDate - StartProject;
+
+                // Calculate the progress as a percentage
+                int progress = (int)((elapsedDuration.TotalDays / projectDuration.TotalDays) * 1000);
+
+                // Make sure the progress value is within the valid range of 0-100
+                return Math.Max(0, Math.Min(progress, 100));
+            }
+        }
     }
 }
