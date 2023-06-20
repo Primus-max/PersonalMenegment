5456b502012c65d82bfe9cbe620664676aedc83e
Vladimir <urkytsk3@gmail.com>
2023-06-21 07:19:53 +0800

    Добавил модель DepartmentStatistics

---
+++ PersonnelManagement/Model/DepartmentStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelManagement.Model
+{
+    public class DepartmentStatistics
+    {
+        public string DepartmentName { get; set; }
+        public int EmployeeCount { get; set; }
+        public decimal TotalProfit { get; set; }
+        public decimal Budget { get; set; }
+        public decimal Efficiency { get; set; }
+    }
+}
